39c0af1b8e7bff98ef1aeefb4f2a6fa1bba68361
Joonazboy <joonas1.syrjanen@gmail.com>
2018-12-21 15:29:16 +0200

    Update cachematrix.R
    Filled out both functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+#makeCacheMatrix gets a matrix as a parameter and creates a variable z to store an inverse of that matrix. 
+#A set of functions will also be created that return or set the inverse matrix. These will be stored in a list from where these sub-functions can be called.
+# the set sub-function takes y as a parameter and replaces the matrix x in the outer environment with y and sets z in the outer environment as NULL
 makeCacheMatrix <- function(x = matrix()) {
-
+    z = NULL
+    set <- function(y){
+        x<<-y
+        z<<-NULL
+    }
+    get<- function()x
+    setinverse <- function(inverse) z <<- inverse
+    getinverse <- function() z
+    list(setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
 
+# Cache solve gets an object x that is created by giving makeCacheMatrix a matrix. 
+# The inverse of this matrix is assigned to variable Z. This is done by calling the getInverse fuction on the parameter x.
+# If there already exists an inverse matrix z will be returned. If not, the matrix will be assigned from x to variable called "matrix" and the inverse will be solved. After this the inverse matrix will be stored in the x object.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        z <- x$getInverse()
+        if(!is.null(z)){
+            print("was not null")
+            return(z)
+        }
+        matrix <- x$get()
+        z <- solve(matrix)
+        x$setInverse(z)
+        z
 }
